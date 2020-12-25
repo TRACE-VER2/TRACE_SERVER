f468492504f2fea344a4d72ee5a82ae56351aabe
syleemk <syleemk@gmail.com>
2020-12-26 02:17:10 +0900

    [MODIFY] 권한 부여 방식 변경 (PreAuthorize 어노테이션)

--- src/main/java/com/trace/traceproject/config/SecurityConfig.java
+++ src/main/java/com/trace/traceproject/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@EnableGlobalMethodSecurity(prePostEnabled = true) //어노테이션으로 권한 설정
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
@@ -46,13 +48,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic().disable() //rest api이므로 기본설정 사용안함. 기본설정은 비인증시 로그인폼으로 리다이렉트 됨
                 .csrf().disable() //rest api이므로 csrf보안이 필요없으므로 diable
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //jwt 토큰기반 인증이므로 세션 필요없기에 생성안함
-                .and()
-                    .authorizeRequests() //요청에 대한 사용권한 체크
-                        .antMatchers("/api/v1/members/login", "/api/v1/members/join", "/api/v1/members/refreshToken").permitAll() //가입 및 인증 주소는 누구나 접근 가능
-                        .antMatchers("/api/v1/mail/**").permitAll() //인증 이메일 전송, 비밀번호 찾기 이메일 전송
-                    .antMatchers(HttpMethod.GET, "/exception/**").permitAll()
-                    .antMatchers("/api/v1/images/**").hasRole("ADMIN") //접근권한 테스트용 설정
-                    .anyRequest().hasRole("USER") //나머지 요청은 모두 인증된 회원만 접근 가능
+//                .and()
+//                    .authorizeRequests() //요청에 대한 사용권한 체크
+//                        .antMatchers("/api/v1/members/login", "/api/v1/members/join", "/api/v1/members/refreshToken").permitAll() //가입 및 인증 주소는 누구나 접근 가능
+//                        .antMatchers("/api/v1/mail/**").permitAll() //인증 이메일 전송, 비밀번호 찾기 이메일 전송
+//                    .antMatchers(HttpMethod.GET, "/exception/**").permitAll()
+//                    .antMatchers("/api/v1/images/**").hasRole("ADMIN") //접근권한 테스트용 설정
+//                    .anyRequest().hasRole("USER") //나머지 요청은 모두 인증된 회원만 접근 가능
                 .and() //exceptionHandling() : 예외처리기능 작동
                     .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler()) //인가 실패시 처리
                 .and()
